c15c481d164f95df1a827c5801ec59278b9f062e
yiguo.wang <yiguo.wang@ly.com>
2024-09-06 16:50:17 +0800

    26. 删除有序数组中的重复项

--- SwiftAlgorithm/SwiftAlgorithm/ViewController.swift
+++ SwiftAlgorithm/SwiftAlgorithm/ViewController.swift
@@ -12,10 +12,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        var nums = [1,1,2]
+        print("leetcode before: nums = \(nums)")
+        let k = self.removeDuplicates(&nums)
+        print("leetcode after: nums = \(nums) k = \(k)")
     }
 
-
+    // 26. 删除有序数组中的重复项
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.count == 0 {
+            return 0
+        }
+        
+        if nums.count == 1 {
+            return 1
+        }
+        
+        var i = nums.count - 2
+        var temp = nums[nums.count - 1]
+        while i >= 0 {
+            if nums[i] == temp {
+                nums.remove(at: i)
+            } else {
+                temp = nums[i]
+            }
+            
+            i -= 1
+        }
+        
+        return nums.count
+    }
+    
     /// 合并两个有序数组
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
         var i = m - 1
